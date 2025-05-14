b0184e88811e252afd9a450a97ee56e5c1991b93
RizkiW080410 <rw43917@gmail.com>
2025-05-14 20:54:58 +0700

    frontend

--- backend_magang/src/database/seeders/DatabaseSeeder.php
+++ backend_magang/src/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,40 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-        ]);
+        // $user = \App\Models\User::factory()->create([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@admin.com',
+        // ]);
 
-        $user->assignRole('super_admin');
+        // $user->assignRole('super_admin');
+
+        $this->seedUsers();
+        $this->callSeeders();
+    }
+
+    private function seedUsers(): void
+    {
+        if (! User::where('email', 'admin@admin.com')->exists()) {
+            $users = User::factory()->createMany([
+                [
+                    'name' => 'Admin',
+                    'email' => 'admin@admin.com',
+                    'password' => bcrypt('password'),
+                ],
+            ]);
+
+            foreach ($users as $user) {
+                if ($user->email === 'admin@admin.com') {
+                    $user->assignRole('super_admin');
+                }
+            }
+        }
+    }
+
+    private function callSeeders(): void
+    {
+        $this->call([
+            RoleSeeder::class,
+        ]);
     }
 }
